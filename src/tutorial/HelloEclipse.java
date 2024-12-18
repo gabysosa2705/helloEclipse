78dc45fac3571fff77f6cd32bb0cc563cef2c3fe
gabsostel <gabsostel@lliurex-client23>
2024-12-18 10:31:13 +0100

    Primer Commit

---
+++ src/tutorial/HelloEclipse.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class HelloEclipse {
+
+	public HelloEclipse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
